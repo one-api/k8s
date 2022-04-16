d2a6350eeafa71e3819d110c102ed5ee8b2cac35
one-api <57555932+one-api@users.noreply.github.com>
2022-04-16 12:35:07 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+curl -sSL https://raw.githubusercontent.com/one-api/k8s/main/install.sh | sudo sh
---
+++ install.sh
@@ -0,0 +1,130 @@
+export DEBIAN_FRONTEND=noninteractive
+set -e
+
+apt-get update
+apt-get install -y -qq apt-transport-https ca-certificates curl git gnupg gnupg2 gnupg1 containerd >/dev/null
+
+# install golang
+if ! $(type go >/dev/null 2>&1);then curl -L https://go.dev/dl/go1.20.5.linux-amd64.tar.gz | tar zxf - -C /usr/local; ln -s /usr/local/go/bin/go /usr/local/bin;fi
+
+mkdir -p /etc/apt/keyrings/
+#curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+#echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y -qq kubelet kubeadm kubectl >/dev/null
+apt-mark hold kubelet kubeadm kubectl
+
+modprobe overlay
+modprobe br_netfilter
+sysctl -w net.bridge.bridge-nf-call-iptables=1
+sysctl -w net.bridge.bridge-nf-call-ip6tables=1
+sysctl -w net.ipv4.ip_forward=1
+
+
+git clone https://github.com/containerd/containerd.git
+sh -c 'cd containerd/ && bash script/setup/install-cni'
+
+
+mkdir -p /etc/containerd
+cat << EOF > /etc/containerd/config.toml
+version = 2
+root = "/var/lib/containerd"
+state = "/run/containerd"
+oom_score = 0
+# imports
+
+[grpc]
+  address = "/run/containerd/containerd.sock"
+  uid = 0
+  gid = 0
+  max_recv_message_size = 16777216
+  max_send_message_size = 16777216
+
+[debug]
+  address = ""
+  uid = 0
+  gid = 0
+  level = ""
+
+[metrics]
+  address = ""
+  grpc_histogram = false
+
+[cgroup]
+  path = ""
+
+[plugins]
+  [plugins."io.containerd.monitor.v1.cgroups"]
+    no_prometheus = false
+  [plugins."io.containerd.grpc.v1.cri"]
+    stream_server_address = ""
+    stream_server_port = "10010"
+    enable_selinux = false
+    sandbox_image = "registry.k8s.io/pause:3.9"
+    stats_collect_period = 10
+    enable_tls_streaming = false
+    max_container_log_line_size = 16384
+    restrict_oom_score_adj = false
+
+    [plugins."io.containerd.grpc.v1.cri".containerd]
+      discard_unpacked_layers = true
+      snapshotter = "overlayfs"
+      default_runtime_name = "runc"
+      [plugins."io.containerd.grpc.v1.cri".containerd.untrusted_workload_runtime]
+        runtime_type = ""
+        runtime_engine = ""
+        runtime_root = ""
+      [plugins."io.containerd.grpc.v1.cri".containerd.runtimes]
+        [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
+          runtime_type = "io.containerd.runc.v2"
+          [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
+            SystemdCgroup = true
+
+    [plugins."io.containerd.grpc.v1.cri".cni]
+      bin_dir = "/opt/cni/bin"
+      conf_dir = "/etc/cni/net.d"
+      conf_template = ""
+    [plugins."io.containerd.grpc.v1.cri".registry]
+      config_path = "/etc/containerd/certs.d"
+
+  [plugins."io.containerd.service.v1.diff-service"]
+    default = ["walking"]
+  [plugins."io.containerd.gc.v1.scheduler"]
+    pause_threshold = 0.02
+    deletion_threshold = 0
+    mutation_threshold = 100
+    schedule_delay = "0s"
+    startup_delay = "100ms"
+EOF
+
+service containerd restart
+
+
+
+#https://kubernetes.io/docs/tasks/administer-cluster/migrating-from-dockershim/troubleshooting-cni-plugin-related-errors/#an-example-containerd-configuration-file
+# cat <<EOF >kubeadm-config.yaml
+# kind: ClusterConfiguration
+# apiVersion: kubeadm.k8s.io/v1beta3
+# kubernetesVersion: v1.21.0
+# ---
+# kind: KubeletConfiguration
+# apiVersion: kubelet.config.k8s.io/v1beta1
+# cgroupDriver: systemd
+# EOF
+
+#kubeadm init --config kubeadm-config.yaml
+kubeadm init
+
+rm -rf ./containerd/
+
+mkdir -p $HOME/.kube
+cp -f /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl get all -nkube-system
+
+# dashboard
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
+# http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/.
